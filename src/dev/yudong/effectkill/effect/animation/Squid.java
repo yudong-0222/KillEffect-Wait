ef4b16424f27272391f5479135ad3d984dfc38e5
RedFeaTherXll <yuchi9716@gmail.com>
2023-07-01 00:15:52 +0800

    重新編寫火箭系統 D:

--- src/dev/yudong/effectkill/effect/animation/Squid.java
+++ src/dev/yudong/effectkill/effect/animation/Squid.java
@@ -3,6 +3,7 @@ package dev.yudong.effectkill.effect.animation;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -28,32 +29,70 @@ public class Squid extends MainEffectKill{
 	@Override
 	public void update(User user) {
 		Location loc = user.getPlayer().getLocation().add(0, -0.3, 0);
-		ArmorStand armor = (ArmorStand)loc.getWorld().spawnEntity(loc.add(0, -1, 0), EntityType.ARMOR_STAND);
-		armor.setVisible(false);
-		armor.setGravity(false);
-		Entity e = user.getPlayer().getWorld().spawnEntity(loc, EntityType.SQUID);
-		armor.setPassenger(e);
-		as.add(armor);
-		new BukkitRunnable() {
-			int i = 0;
-			@Override
-			public void run() {
-				i++;
-				Entity passenger = armor.getPassenger();
-				armor.eject();
-				armor.teleport(armor.getLocation().add(0,0.5,0));
-				armor.setPassenger(passenger);
-				Particle.play(armor.getLocation().add(0.0, -0.2, 0.0), Effect.FLAME);
-				user.getPlayer().playSound(loc, Sound.CHICKEN_EGG_POP, 1f, 1f);
-				if(i == 20) {
-					as.remove(armor);
-					armor.remove();
-					e.remove();
-					Particle.play(armor.getLocation().add(0.0, 0.5, 0.0), Effect.EXPLOSION_HUGE, 1);
-					i = 0;
-					cancel();
-				} 
-			}
-		}.runTaskTimer(Main.getInstance(), 1, 0);
+
+		Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+			ArmorStand armor = (ArmorStand) loc.getWorld().spawnEntity(loc.add(0, -1, 0), EntityType.ARMOR_STAND);
+			armor.setVisible(false);
+			armor.setGravity(false);
+			Entity e = user.getPlayer().getWorld().spawnEntity(loc, EntityType.SQUID);
+			armor.setPassenger(e);
+			as.add(armor);
+
+			new BukkitRunnable() {
+				int i = 0;
+
+				@Override
+				public void run() {
+					i++;
+					Entity passenger = armor.getPassenger();
+					armor.eject();
+					armor.teleport(armor.getLocation().add(0, 0.5, 0));
+					armor.setPassenger(passenger);
+					Particle.play(armor.getLocation().add(0.0, -0.2, 0.0), Effect.FLAME);
+					user.getPlayer().playSound(loc, Sound.CHICKEN_EGG_POP, 1f, 1f);
+					if (i == 20) {
+						as.remove(armor);
+						armor.remove();
+						e.remove();
+						Particle.play(armor.getLocation().add(0.0, 0.5, 0.0), Effect.EXPLOSION_HUGE, 1);
+						i = 0;
+						cancel();
+					}
+				}
+			}.runTaskTimer(Main.getInstance(), 1, 0);
+		});
 	}
+
+
+//	@Override
+//	public void update(User user) {
+//		Location loc = user.getPlayer().getLocation().add(0, -0.3, 0);
+//		ArmorStand armor = (ArmorStand)loc.getWorld().spawnEntity(loc.add(0, -1, 0), EntityType.ARMOR_STAND);
+//		armor.setVisible(false);
+//		armor.setGravity(false);
+//		Entity e = user.getPlayer().getWorld().spawnEntity(loc, EntityType.SQUID);
+//		armor.setPassenger(e);
+//		as.add(armor);
+//		new BukkitRunnable() {
+//			int i = 0;
+//			@Override
+//			public void run() {
+//				i++;
+//				Entity passenger = armor.getPassenger();
+//				armor.eject();
+//				armor.teleport(armor.getLocation().add(0,0.5,0));
+//				armor.setPassenger(passenger);
+//				Particle.play(armor.getLocation().add(0.0, -0.2, 0.0), Effect.FLAME);
+//				user.getPlayer().playSound(loc, Sound.CHICKEN_EGG_POP, 1f, 1f);
+//				if(i == 20) {
+//					as.remove(armor);
+//					armor.remove();
+//					e.remove();
+//					Particle.play(armor.getLocation().add(0.0, 0.5, 0.0), Effect.EXPLOSION_HUGE, 1);
+//					i = 0;
+//					cancel();
+//				}
+//			}
+//		}.runTaskTimer(Main.getInstance(), 1, 0);
+//	}
 }
